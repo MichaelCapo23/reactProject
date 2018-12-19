07fc6df8a2e21307a7758350b1f4491a03d21256
MichaelCapo23 <123pie23@gmail.com>
2018-12-19 10:02:02 -0800

    switched function places start validating answer

--- guessing game/game/src/board.js
+++ guessing game/game/src/board.js
@@ -9,6 +9,7 @@ class Board extends Component {
             cup2: "position2",
             cup3: "position3",
             count: 0,
+            userGuess: '',
         };
 
 
@@ -39,28 +40,48 @@ class Board extends Component {
         window.setTimeout(this.switchPositions, 500);
     };
 
-    compareValues = () => {
-
-    };
-
     resetState = () => {
         this.setState({
             cup1: "position1",
             cup2: "position2",
             cup3: "position3",
             count: 0
-        })
+        });
+    };
+
+    handleChange = (event) => {
+        console.log(event.target.value);
+        event.preventDefault();
+        this.setState({
+          userGuess: event.target.value
+        });
+    };
+
+    submitted = (event) => {
+        event.preventDefault();
+        console.log("input values: ", this.state);
+
+        this.setState({
+            cup1: "position1",
+            cup2: "position2",
+            cup3: "position3",
+            count: 0,
+            userGuess: '',
+        });
     };
 
     render = () => {
         const {cup1, cup2, cup3} = this.state;
+        console.log(this.state.userGuess);
         return (
             <div className="board">
-                <Cup className={cup1} number={"1"}/>
-                <Cup className={cup2} number={"2"}/>
-                <Cup className={cup3} number={"3"}/>
-                <input className={`input`} placeholder={`guess`}></input>
-                <Button function={this.compareValues} name="submit" text={`submit`}/>
+                <Cup onClick={this.compareValues} userguess={this.state.userGuess} className={cup1} number={"1"}/>
+                <Cup onClick={this.compareValues} userguess={this.state.userGuess} className={cup2} number={"2"}/>
+                <Cup onClick={this.compareValues} userguess={this.state.userGuess} className={cup3} number={"3"}/>
+                    <form onSubmit={this.submitted}>
+                        <input onChange={this.handleChange} className={`input`} placeholder={`guess`}></input>
+                        <Button name="submit" text={`submit`}/>
+                    </form>
                 <Button function={this.resetState} name="reset" text={`reset`}/>
                 <Button function={this.switchPositions} name="start" text={`start`}/>
             </div>
